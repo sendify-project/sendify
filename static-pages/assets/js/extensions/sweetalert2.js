8e7d5b23a6fa7d281e4e94d9debbeef4df445dae
weibig <wendydaan@gmail.com>
2021-05-30 00:43:45 +0800

    upload file

--- static-pages/assets/js/extensions/sweetalert2.js
+++ static-pages/assets/js/extensions/sweetalert2.js
@@ -152,3 +152,27 @@ document.getElementById('select').addEventListener('click', async (e) => {
       }
   
 })
+
+document.getElementById('file').addEventListener('click', async (e) => {
+  // alert("HI");
+  const { value: file } = await Swal.fire({
+    title: 'Select image',
+    input: 'file',
+    inputAttributes: {
+      'accept': 'image/*',
+      'aria-label': 'Upload your profile picture'
+    }
+  })
+  
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      Swal.fire({
+        title: 'Your uploaded picture',
+        imageUrl: e.target.result,
+        imageAlt: 'The uploaded picture'
+      })
+    }
+    reader.readAsDataURL(file)
+  }
+})

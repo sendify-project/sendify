7f50eb8e7a99b8add36cc6802b32754e0eae00f5
disam8853 <wang.sam33@gmail.com>
2021-06-06 06:20:30 +0800

    add chat client to drone

--- chat-server/app.js
+++ chat-server/app.js
@@ -1,12 +1,9 @@
 var express = require('express')
-var path = require('path')
+// var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-const { createProxyMiddleware } = require('http-proxy-middleware')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
-
-const ACCOUNT_API = process.env.ACCOUNT_API
 
 var apiRouter = require('./routes/api')
 
@@ -16,9 +13,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', apiRouter)
-// app.use('/api', createProxyMiddleware({ target: ACCOUNT_API, changeOrigin: true }))
 
 module.exports = app

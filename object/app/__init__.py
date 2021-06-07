b16a2e9a33f4dcfa4f60e7391218c6af74c73348
wendy.chen <wendy.chen@linecorp.com>
2021-06-07 18:06:23 +0800

    save to s3 done

--- object/app/__init__.py
+++ object/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app import views
-from .views import search
+from .views import upload
 
 app = Flask(__name__)
-app.register_blueprint(search)
+app.register_blueprint(upload)
--- object/app/views.py
+++ object/app/views.py
@@ -1,12 +1,61 @@
-from flask import url_for, redirect, render_template, flash, g, session
-from app import app
+from flask import Blueprint, request, make_response
+import requests
+import io
+import os
+from botocore.client import Config
+import boto3
+import uuid
+import datetime
+import PIL.Image as Image
+
+upload = Blueprint('upload', __name__, url_prefix='/')
 
 
-@app.route('/upload')
-def upload():
-    return render_template('index.html')
+@upload.route('/upload', methods=["POST"])
+def files():
+    file = request.files['file']
 
+    user_id = request.headers.get('X-User-Id')
+    channel_id = request.headers.get('X-Channel-Id')
 
-@app.route('/download')
-def download():
-    return render_template('list.html')
+    access_key = os.getenv("ACCESS_KEY", "XJ2hzpkN")
+    secret_key = os.getenv("SECRET_KEY", "LkYatYm5Ke4BpnQk")
+    expire_days = int(os.getenv("EXPIRE_DAYS", "10"))
+    s3_host = os.getenv("S3_HOST", 'http://linux1.csie.ntu.edu.tw:8009')
+
+    config = Config(signature_version='s3',
+                    s3={'addressing_style': 'path'})
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key,
+                        endpoint_url=s3_host,
+                        config=config)
+
+    _, file_ext = os.path.splitext(file.filename)
+    file_ext = file_ext.lower().replace("jpg", "jpeg")
+    img_ext_list = [".png", ".jpg", ".jpeg"]
+    fid = str(uuid.uuid4())
+    print(file_ext)
+    if file_ext in img_ext_list:
+        content_type = "image/"+file_ext
+        object_type = "img"
+    else:
+        content_type = "application/octet-stream"
+        object_type = "file"
+
+    response = s3.Bucket('sendify-object').put_object(Key=fid, Body=file,
+                                                      ACL='public-read', Expires=datetime.datetime.today()+datetime.timedelta(days=expire_days), ContentType=content_type).get()  # ['ResponseMetadata']['HTTPStatusCode']
+    if not response:
+        make_response("Something went wrong when uploading to s3", 400)
+
+    data = {
+        "type": object_type,
+        "s3_url": s3_host+"/sendify-object/"+fid,
+        "orginal_filename": file.filename
+    }
+    header = {"X-User-Id": user_id, "X-Channel-Id": channel_id}
+
+    # TODO
+    # resp = requests.post("", headers=header, data=data)
+    # return make_response(resp.json(), resp.status_code)
+    return make_response({"headers": header, "data": data}, 200)

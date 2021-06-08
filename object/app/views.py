d4eb0ae211d447f17c4f04fdc9b4be79b577d5be
wendy.chen <wendy.chen@linecorp.com>
2021-06-08 18:20:34 +0800

    add sample drone/swagger file

--- object/app/views.py
+++ object/app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, current_app
 import requests
 import io
 import os
@@ -18,10 +18,11 @@ def files():
     user_id = request.headers.get('X-User-Id')
     channel_id = request.headers.get('X-Channel-Id')
 
-    access_key = os.getenv("ACCESS_KEY")
-    secret_key = os.getenv("SECRET_KEY")
-    expire_days = int(os.getenv("EXPIRE_DAYS", "10"))
-    s3_host = os.getenv("S3_HOST")
+    access_key = current_app.config.get("ACCESS_KEY")
+    secret_key = current_app.config.get("SECRET_KEY")
+    expire_days = int(current_app.config.get("EXPIRE_DAYS", "10"))
+    s3_host = current_app.config.get("S3_HOST")
+    print(access_key, secret_key, expire_days, s3_host)
 
     config = Config(signature_version='s3',
                     s3={'addressing_style': 'path'})
@@ -35,22 +36,21 @@ def files():
     file_ext = file_ext.lower().replace("jpg", "jpeg")
     img_ext_list = [".png", ".jpg", ".jpeg"]
     fid = str(uuid.uuid4())
-    print(file_ext)
+
     if file_ext in img_ext_list:
-        content_type = "image/"+file_ext
+        content_type = "image/" + file_ext
         object_type = "img"
     else:
         content_type = "application/octet-stream"
         object_type = "file"
 
-    response = s3.Bucket('sendify-object').put_object(Key=fid, Body=file,
-                                                      ACL='public-read', Expires=datetime.datetime.today()+datetime.timedelta(days=expire_days), ContentType=content_type).get()  # ['ResponseMetadata']['HTTPStatusCode']
+    response = s3.Bucket('sendify-object').put_object(Key=fid, Body=file, ACL='public-read', Expires=datetime.datetime.today() + datetime.timedelta(days=expire_days), ContentType=content_type).get()  # ['ResponseMetadata']['HTTPStatusCode']
     if not response:
         make_response("Something went wrong when uploading to s3", 400)
 
     data = {
         "type": object_type,
-        "s3_url": s3_host+"/sendify-object/"+fid,
+        "s3_url": s3_host + "/sendify-object/" + fid,
         "orginal_filename": file.filename
     }
     header = {"X-User-Id": user_id, "X-Channel-Id": channel_id}

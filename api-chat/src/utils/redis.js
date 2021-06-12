9c02aadd7d3f93c400cb028f1e7b451f568789a1
disam8853 <wang.sam33@gmail.com>
2021-06-12 20:43:22 +0800

    add redis password

--- api-chat/src/utils/redis.js
+++ api-chat/src/utils/redis.js
@@ -2,22 +2,37 @@ const Redis = require('ioredis')
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const REDIS_ENDPOINT = process.env.REDIS_ENDPOINT
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD
 
 if (!REDIS_ENDPOINT) throw new Error('REDIS_ENDPOINT is not found!')
 
 const redis = {
-  pub: new Redis.Cluster([
+  pub: new Redis.Cluster(
+    [
+      {
+        port: parseInt(REDIS_ENDPOINT.split(':')[1]),
+        host: REDIS_ENDPOINT.split(':')[0],
+      },
+    ],
     {
-      port: parseInt(REDIS_ENDPOINT.split(':')[1]),
-      host: REDIS_ENDPOINT.split(':')[0],
-    },
-  ]),
-  sub: new Redis.Cluster([
+      redisOptions: {
+        password: REDIS_PASSWORD,
+      },
+    }
+  ),
+  sub: new Redis.Cluster(
+    [
+      {
+        port: parseInt(REDIS_ENDPOINT.split(':')[1]),
+        host: REDIS_ENDPOINT.split(':')[0],
+      },
+    ],
     {
-      port: parseInt(REDIS_ENDPOINT.split(':')[1]),
-      host: REDIS_ENDPOINT.split(':')[0],
-    },
-  ]),
+      redisOptions: {
+        password: REDIS_PASSWORD,
+      },
+    }
+  ),
 }
 
 module.exports.default = redis

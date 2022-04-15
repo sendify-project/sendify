b301162310bf5ec75746794fc4413841eb120519
ming.hsu <ming.hsu@linecorp.com>
2022-04-15 20:51:49 +0800

    remove unused button

--- web-client/src/components/login-page.js
+++ web-client/src/components/login-page.js
@@ -75,12 +75,6 @@ function LoginPage({ setUser, getUserInfo, logout }) {
                 <i className='bi bi-shield-lock'></i>
               </div>
             </div>
-            <div className='form-check form-check-lg d-flex align-items-end'>
-              <input className='form-check-input me-2' type='checkbox' value='' id='flexCheckDefault' />
-              <label className='form-check-label text-gray-600' htmlFor='flexCheckDefault'>
-                Keep me logged in
-              </label>
-            </div>
             <button className='btn btn-primary btn-block btn-lg shadow-lg mt-5' onClick={handleClick}>
               Log in
             </button>
